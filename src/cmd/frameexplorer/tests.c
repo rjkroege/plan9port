5bdf58f4c06317fd426fdaf146fd7ba852b94fb2
Robert Kroeger <rjkroege@liqui.org>
2011-12-15 09:14:26 -0500

    more test framework action

--- src/cmd/frameexplorer/tests.c
+++ src/cmd/frameexplorer/tests.c
@@ -7,8 +7,31 @@
 #include <cursor.h>
 #include <frame.h>
 
+/*
+	Design pattern for tests: Return an int: true on success, 0 on fail.
+	Generate no output on success. Generate lots of output on fail.
+*/
+int
+testOne(Frame *f)
+{
+	print("Unimplemented test: fail\n");
+	return 0;
+}
+
+
+
+
+
+
 void
 runAllTests(Frame* f)
 {
-	print("Run some tests here\n");
+	int success = 1;
+	success = success && testOne(f);
+	// Insert more here.
+	
+	if (!success) {
+		print("FAIL\n");
+		threadexitsall("Test failure");
+	}
 }
